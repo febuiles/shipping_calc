c02110df076822c579f8ca5ed2310cf166484cf0
Federico Builes <federico@korobeiniki.(none)>
2008-03-13 19:55:08 -0500

    What was that doing here?

--- lib/shipping_calc/foo.rb
+++
@@ -1,7 +0,0 @@
-    def shipment_item(dimensions)
-      raise StandardError.new("Invalid shipment dimensions") unless (dimensions =~ /\d+x\d+x\d+/)
-
-      d = dimensions.split("x")
-    end
-
-shipment_item("23x3")
