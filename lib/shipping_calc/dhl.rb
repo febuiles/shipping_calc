d4aa0cc50a62cf5473f79b79f602cd19ae8fe5d6
Federico Builes <federico.builes@gmail.com>
2008-06-24 00:51:59 -0500

    Improvements for DHL
    * DHL calculations now handle DHL errors and print a descriptive
      error when the quote fails.
    * Default date value (Time.now) is now correctly calculated.

--- lib/shipping_calc/dhl.rb
+++ lib/shipping_calc/dhl.rb
@@ -26,13 +26,21 @@ require 'uri'
 include REXML
 
 module ShippingCalc
-
   # Current version on their website is 1.0 and can be found in:
   # https://eCommerce.airborne.com/ApiLandingTest.asp . To get full access to
   # all their stuff you have to make sure they certify your application
   # against their live platform tests. The test bed should be enough to get
   # simple calculations.
   # Currently, only shipments made inside the US are available.
+
+  US_STATES =  ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC',
+               'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN',
+               'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN',
+               'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ',
+               'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI',
+               'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA',
+               'WI', 'WV'] 
+
   class DHL
 
     # Obtains an estimate quote from the DHL site. 
@@ -53,6 +61,7 @@ module ShippingCalc
     def quote(params)
       @xml = xml = Document.new
       xml << XMLDecl.new("1.0' encoding='UTF-8")
+      raise ShippingCalcError.new("Invalid parameters") if params.nil?
       raise ShippingCalcError.new("Missing shipping parameters") unless params.keys.length == 10
       auth(params[:api_user], params[:api_password])
       rate_estimate(params)
@@ -60,25 +69,32 @@ module ShippingCalc
     end
 
     private 
-    # DHL gets the quotes in 2 steps, the first one is authentication. This
+
+    # DHL gets the quote in 2 steps, the first one is authentication. This
     # generates the XML for that.
     def auth(api_user, api_password)
-      ecomm = Element.new "eCommerce"
-      ecomm.attributes["action"] = "Request"
-      ecomm.attributes["version"] = "1.1"
+      # <eCommerce action="Request" version="1.1">
+      #   <Requestor>
+      #     <ID>username</ID>
+      #     <Password>password</Password>
+      #   </Requestor>
+      # </eCommerce>
+      ecommerce = Element.new "eCommerce"
+      ecommerce.attributes["action"] = "Request"
+      ecommerce.attributes["version"] = "1.1"
 
       user = Element.new "Requestor"
-      u_id = Element.new "ID"
-      u_id.text = api_user 
+      user_id = Element.new "ID"
+      user_id.text = api_user 
 
-      u_pwd = Element.new "Password"
-      u_pwd.text = api_password 
+      user_pwd = Element.new "Password"
+      user_pwd.text = api_password 
 
-      user << u_id
-      user << u_pwd
+      user << user_id
+      user << user_pwd
 
-      ecomm << user
-      @xml << ecomm
+      ecommerce << user
+      @xml << ecommerce
     end
 
     # After having the auth message ready, we create the RateEstimate request.
@@ -96,14 +112,20 @@ module ShippingCalc
     #     to_country: Recipient's country. Not used, currently DHL only supports US.
     #     to_state: Recipient's state.
     def rate_estimate(params)
+
+      # <Shipment action="RateEstimate" version="1.0">
       shipment = Element.new "Shipment"
       shipment.attributes["action"] = "RateEstimate"
       shipment.attributes["version"] = "1.0"
 
+      # <ShippingCredentials>
+      #   <ShippingKey>key</ShippingKey> 
+      #   <AccountNbr>number</AccountNbr>
+      # </ShippingCredentials>
       credentials = Element.new "ShippingCredentials"
+
       key = Element.new "ShippingKey"
       key.text = params[:shipping_key] 
-
       account = Element.new "AccountNbr"
       account.text = params[:account_num] 
 
@@ -111,50 +133,84 @@ module ShippingCalc
       credentials << account
       shipment << credentials
 
-      detail = Element.new "ShipmentDetail"
-      date = Element.new "ShipDate"   
-
-      date.text = date(params[:date])
-      detail << date
+      # <ShipmentDetail>
+      #   <ShiptDate>date</ShipDate>
+      shipment_detail = Element.new "ShipmentDetail"
+      ship_date = Element.new "ShipDate"   
+      ship_date.text = date(params[:date])
+      shipment_detail << ship_date
 
       # TODO: Implement SAT and 1030 services
+
+      #   <Service>
+      #     <Code>code</Code>
+      #   </Service>
       service = Element.new "Service"
-      s_code = Element.new "Code"
-      s_code.text = service_code(params[:service_code])
-      detail << service << s_code
+      service_code = Element.new "Code"
+      service_code.text = service_code(params[:service_code])
+      service << service_code
 
-      type = Element.new "ShipmentType"
-      t_code = Element.new "Code"
-      t_code.text = shipment_code(params[:shipment_code])
-      detail << type << t_code
+      shipment_detail << service
 
+      #   <ShipmentType>
+      #     <Code>code</Code>
+      #   </ShipmentType>
+      shipment_type = Element.new "ShipmentType"
+      shipment_type_code = Element.new "Code"
+      shipment_type_code.text = shipment_code(params[:shipment_code])
+      shipment_type << shipment_type_code
+      shipment_detail << shipment_type
+
+      #   <Weight>weight</Weight>
       weight = Element.new "Weight"
       weight.text = weight(params[:weight], params[:shipment_code])
-      shipment << detail << weight
 
+      # </ShipmentDetail>      
+      shipment_detail << weight
+
+      shipment << shipment_detail
+
+      # <Billing>
+      #   <Party>
+      #     <Code>S</Code>
+      #   </Party>
+      # </Billing>
       billing = Element.new "Billing"
-      b_party = Element.new "Party"
-      p_code = Element.new "Code"
+      billing_party = Element.new "Party"
+      billing_party_code = Element.new "Code"
+
       # Since we're just doing some quick calulations we don't want to be
       # worrying about who's gonna send the package. Just make the calulations 
       # assuming the sender pays for the shipping.
-      p_code.text = "S"
-      shipment << billing << b_party << p_code
+      billing_party_code.text = "S"
 
+      billing << billing_party << billing_party_code
+
+      shipment << billing
+
+      # <Receiver>
+      #   <Address>
+      #     <State>state</State>
+      #     <Country>country</Country>
+      #     <PostalCode>code</PostalCode>
+      #   </Address>
+      # </Receiver>
       receiver = Element.new "Receiver"
-      r_addr = Element.new "Address"
-      r_state = Element.new "State"
-      r_country = Element.new "Country"
-      r_zipcode = Element.new "PostalCode"
+      receiver_addr = Element.new "Address"
+      receiver_state = Element.new "State"
+      receiver_country = Element.new "Country"
+      receiver_zipcode = Element.new "PostalCode"
 
-      r_state.text = state(params[:to_state])
-      r_country.text = "US"
-      r_zipcode.text = zip_code(params[:to_zip])
+      receiver_state.text = state(params[:to_state])
+      receiver_country.text = "US"
+      receiver_zipcode.text = zip_code(params[:to_zip])
 
-      r_addr << r_state
-      r_addr << r_country
-      r_addr << r_zipcode
-      shipment << receiver << r_addr
+      receiver_addr << receiver_state
+      receiver_addr << receiver_country
+      receiver_addr << receiver_zipcode
+      receiver << receiver_addr
+
+      shipment << receiver
 
       root = @xml.elements["eCommerce"]
       root.add shipment
@@ -174,12 +230,11 @@ module ShippingCalc
     # Parses the server's response. Currently, it only returns the estimate
     # value of the shipping.
     def parse_response(resp)
-      p resp
+#      p resp
       doc = Document.new(resp)
-      if resp =~ /Fault/
-        raise ShippingCalcError.new("DHL's system is currently offline.")
-        return
-      end
+
+      find_error_and_raise(doc) if errors_exist?(doc)
+
       result =  doc.elements["//Shipment/Result/Desc"].text 
       
       if result == "Shipment estimate successful."
@@ -189,8 +244,45 @@ module ShippingCalc
       end
     end
 
+    def errors_exist?(response)
+      not response.elements["//Faults"].nil?
+    end
+
+    def find_error_and_raise(response)
+    error_code = response.elements["//Code"]
+
+        # Special Services and Additional Protection are not supported        
+      case error_code.text.to_i
+          when 1000..1009 then msg = "Shipment Headers"
+          when 4000..4004 then msg = "ShippingKey"
+          when 4007       then msg = "Account Number"
+          when 4195..4198 then msg = "Account Number"
+          when 4100..4106 then msg = "Shipment Date"
+          when 4108..4117 then msg = "Service Type (Code)"
+          when 4118..4122 then msg = "Shipment Type Code"
+          when 4123..4124 then msg = "Weight"
+          when 4128..4131 then msg = "Dimensions"
+          when 4116       then msg = "Billing Party (Code)"
+          when 4147       then msg = "Billing Party (Code)"
+          when 4149..4152 then msg = "Billing Account Number"
+          when 4164..4166 then msg = "Receiver City"
+          when 4167       then msg = "Receiver State"
+          when 4164..4166 then msg = "Receiver City"
+          when 4169       then msg = "Receiver Country"
+          when 4170..4176 then msg = "Receiver Postal Code"
+          else                 msg = "API Request"
+      end
+
+      raise ShippingCalcError.new("DHL Error #{error_code.text}: Invalid #{msg}")
+    end
+
     def date(date)
-      if date.strftime("%A") == "Sunday"
+      date ||= Time.now
+      if date.kind_of?(String) && date =~ /\d{4}-\d{2}-\d{2}/ # Suppose it's valid
+        return date
+      end
+
+      if date.strftime("%A") == "Sunday"    
         (date + 86400).strftime("%Y-%m-%d") # DHL doesn't ship on Sundays, add 1 day.
       else
         date.strftime("%Y-%m-%d")
